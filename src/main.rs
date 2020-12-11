5d82586ffc281fdd122987afd8ddafa4393c26e0
Michael Paul <michael@michaelpaul.com.br>
2020-12-11 20:05:31 +0000

    Build before running

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use rand::prelude::*;
+
 fn main() {
     let name = ["mundo"];
     let job = std::thread::spawn(move || println!("Olá, {}!", name[0]));
     job.join().expect("unable to fork");
+    println!("Here it's a random number for you: {}", random::<u8>());
     println!("Adios!");
 }
