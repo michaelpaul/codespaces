0c3c268e824e1d87e208287654b265d789d58288
Michael Paul <michael@michaelpaul.com.br>
2020-12-11 20:11:43 +0000

    Cleanup main branch

--- rust-app/src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let name = ["mundo"];
-    let job = std::thread::spawn(move || println!("Olá, {}!", name[0]));
-    job.join().expect("unable to fork");
-    println!("Adios!");
-}
