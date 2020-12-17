f094459e71e508f099f1190f1538bc2a3bdd4363
Michael Paul <michael@michaelpaul.com.br>
2020-12-17 19:59:18 +0000

    Guess game

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
-use rand::prelude::*;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io::{self, Write};
 
 fn main() {
-    let name = ["mundo"];
-    let job = std::thread::spawn(move || println!("Olá, {}!", name[0]));
-    job.join().expect("unable to fork");
-    println!("Here it's a random number for you: {}", random::<u8>());
-    println!("Adios!");
+    let secret = rand::thread_rng().gen_range(1, 100);
+
+    loop {
+        print!("Chute um numero: ");
+        io::stdout().flush().unwrap();
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Como assim falhou?");
+        let guess: u16 = match guess.trim().parse() {
+            Ok(n) => n,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Equal => {
+                println!("We have a winner!");
+                break;
+            }
+            Ordering::Greater => println!("Too big"),
+        }
+    }
 }
